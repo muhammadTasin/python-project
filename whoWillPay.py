b0b73221dd38d35c902588031938b22236d049f4
Muhammad Tasin <youremail@example.com>
2025-07-03 21:22:12 +0600

    Initial commit

---
+++ whoWillPay.py
@@ -0,0 +1,21 @@
+import random
+user = ["sayem","sobhan","sakib","tasin"]
+
+randomnum = random.randint(0, 3)
+if randomnum == 0 :
+    print("bill will pay by Sayem")
+elif randomnum == 1 :
+    print("bill will pay by Sobhan")
+elif randomnum ==2 :
+    print("bill will pay by Sakib")
+else :
+    print("bill will pay by Tasin")
+
+print("=========")  #print side rakhar jonno bug dekha diyechilo that's why sometimes print hoto na ei line ta
+
+
+   ## method 2
+length = len(user) - 1
+random2 = random.randint(0, length)
+biller = user[random2]
+print(f"Bill will pay by {biller}")
